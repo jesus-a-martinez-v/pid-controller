047c07caf02899ac31f4b0493ef2f9ed5459cb4b
Jesús Martínez <jesus.a.martinez.v@gmail.com>
2017-06-20 23:21:45 -0500

    Update PID.cpp

--- src/PID.cpp
+++ src/PID.cpp
@@ -16,20 +16,12 @@ void PID::Init(double Kp, double Ki, double Kd) {
 	this->p_error = 0;
 	this->i_error = 0;
 	this->d_error = 0;
-
-	previous_time = 0;
 }
 
 void PID::UpdateError(double cte) {
-    double current_time = clock();
-    double dt = (current_time - previous_time)/CLOCKS_PER_SEC;
-    previous_time = current_time;
-
-    std::cout << dt << std::endl;
-
-	d_error = (cte - p_error) / dt; // Because p_error is actually the previous cte
+	d_error = cte - p_error; // Because p_error is actually the previous cte
 	p_error = cte;
-	i_error += cte * dt;
+	i_error += cte;
 	// p_error = - Kp * cte;
 	// i_error = - Ki * (i_error + cte);
 	// d_error = - Kd * (cte - d_error);
